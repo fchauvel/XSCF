7a67299d87a1e8243904e181b2d88c83a807e3a2
fchauvel <franck.chauvel@gmail.com>
2017-04-06 14:57:19 +0200

    Removing console outputs from the tests

--- tests/test_rule_factory.cpp
+++ tests/test_rule_factory.cpp
@@ -20,16 +20,12 @@ TEST(TestCrossover, simple_test) {
   Rule *rule_1 = new Rule({Interval(0,50)}, { 4 }, 1.0, 1.0, 1.0);
   Rule *rule_2 = new Rule({Interval(50, 100)}, { 2 }, 1.0, 1.0, 1.0);
 
-  cout << *rule_1 << endl;
-  cout << *rule_2 << endl;
-  
   Evolution evolution;
 
   vector<Rule*> children = evolution.breed(*rule_1, *rule_2, 1, 2);
 
   Rule *expected_child_A = new Rule({Interval(0, 100)}, {4}, 1.0, 1.0, 1.0);
   Rule *expected_child_B = new Rule({Interval(50, 50)}, {2}, 1.0, 1.0, 1.0); 
-
    
   CHECK(2 == children.size());
   CHECK(*expected_child_A == *children[0]);
